6586faa433410f68ab164e987f312d5c18a3b3e2
韦 前程 <small.students.loming@outlook.com>
2025-08-18 17:34:18 +0800

    菜品业务代码：新增菜品

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,50 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class DishServiceImpl implements DishService {
+
+    @Autowired
+    DishMapper dishMapper;
+
+    @Autowired
+    DishFlavorMapper dishFlavorMapper;
+
+    /**
+     * 新增菜品和对应的口味
+     *
+     * @param dishDTO
+     */
+    @Override
+    @Transactional
+    public void saveWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+
+        //向菜品表插入数据
+        dishMapper.insert(dish);
+
+        Long dishId = dish.getId();
+
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if (flavors != null && !flavors.isEmpty()) {
+            flavors.forEach(df -> df.setDishId(dishId));
+            //向口味表插入数据
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+}
