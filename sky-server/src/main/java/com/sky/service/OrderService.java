bfbb1d9a3c17d9a946e072c7d4371ff6a00555c2
韦 前程 <small.students.loming@outlook.com>
2025-08-27 17:03:50 +0800

    管理端订单业务功能:各个状态的订单数量统计

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -5,6 +5,7 @@ import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.PageResult;
 import com.sky.vo.OrderPaymentVO;
+import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
 
@@ -34,4 +35,6 @@ public interface OrderService {
     void repetition(Long id);
 
     PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    OrderStatisticsVO statistics();
 }
