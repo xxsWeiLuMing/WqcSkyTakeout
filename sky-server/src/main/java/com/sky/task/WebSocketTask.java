138a252284c63054c5948bd0cf4cfb9ac1629ed2
韦 前程 <small.students.loming@outlook.com>
2025-08-28 15:54:50 +0800

    来单提醒业务代码

--- sky-server/src/main/java/com/sky/task/WebSocketTask.java
+++ sky-server/src/main/java/com/sky/task/WebSocketTask.java
@@ -1,3 +1,4 @@
+/*
 package com.sky.task;
 
 import com.sky.websocket.WebSocketServer;
@@ -12,11 +13,14 @@ public class WebSocketTask {
     @Autowired
     private WebSocketServer webSocketServer;
 
-    /**
+    */
+/**
      * 通过WebSocket每隔5秒向客户端发送消息
-     */
+     *//*
+
     @Scheduled(cron = "0/5 * * * * ?")
     public void sendMessageToClient() {
         webSocketServer.sendToAllClient("这是来自服务端的消息：" + DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now()));
     }
 }
+*/
